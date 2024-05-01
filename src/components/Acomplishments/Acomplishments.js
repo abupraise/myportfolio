5b0c6647d7f755c22a6fb5bd261f0843ba205551
abupraise <praiseabu2000@gmail.com>
2024-05-01 11:10:30 +0100

    Updating portfolio snippet

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,10 +4,9 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 10, text: 'Projects completed'},
+  { number: 50, text: 'Satisfied clients', },
+  { number: 15, text: 'Technology stacks', }
 ];
 
 const Acomplishments = () => (
